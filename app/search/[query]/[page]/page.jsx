4229baf99c7223ca0b21c04c67975a32c8dcd249
Jignasu Pathak <jignasupathak@Jignasus-MacBook-Pro.local>
2023-06-23 17:31:40 -0400

    Added pagination and comments on the code

---
+++ app/search/[query]/[page]/page.jsx
@@ -0,0 +1,146 @@
+// Import icons from Heroicons package for usage in the component
+import {
+    EyeIcon,
+    HandThumbUpIcon,
+    ArrowDownTrayIcon,
+  } from "@heroicons/react/24/outline";
+  
+  // Asynchronous function to fetch data from the Pixabay API
+  async function getData(query, page) {
+    // Send a GET request to the Pixabay API
+    const res = await fetch(
+      `https://pixabay.com/api/?key=${
+        process.env.PIXABAY_API_KEY // Use the Pixabay API key from environment variables
+      }&q=${decodeURIComponent(query)}&image_type=photo&page=${page}`
+    );
+  
+    // If the response is not OK (status code is not in the range 200-299), return null
+    if (!res.ok) {
+      return null;
+    }
+  
+    // If the response is OK, parse it as JSON and return the result
+    return res.json();
+  }
+  
+  // Function to handle page change, takes an offset and returns a function that changes the page by the offset when called
+  const handlePageChange = (offset) => () => {
+    router.push(`/search/${query}/${page + offset}`);
+  };
+  
+  // Asynchronous function component to render the search query
+  async function SearchQuery({ params: { query, page } }) {
+    // Log the decoded query string for debugging purposes
+    console.log(decodeURIComponent(query));
+  
+    // Fetch data from the Pixabay API
+    const data = await getData(query, page);
+  
+    // Log the fetched data for debugging purposes
+    console.log(data);
+  
+    // Render the search results
+    return (
+      <div className="bg-gray-100 min-h-screen px-2 py-10">
+        {/* Render a header indicating that these are the search results */}
+        <h1 className="text-4xl font-bold text-center mb-6 text-indigo-600">
+          Results
+        </h1>
+  
+        {/* Container for the search results */}
+        <div className="flex flex-wrap justify-center">
+          {/* Iterate over the hits in the fetched data, if it exists */}
+          {data?.hits.map((hit) => (
+            // Render a card for each hit
+            <div
+              key={hit.id} // Use the hit's id as the key
+              className="max-w-sm rounded overflow-hidden shadow-lg m-4" // Style the card
+            >
+              {/* Render an image for the hit */}
+              <img className="w-full" src={hit.previewURL} alt={hit.tags} />
+  
+              {/* Container for the hit's details */}
+              <div className="px-6 py-4">
+                {/* Render the user who uploaded the hit */}
+                <div className="font-bold text-xl mb-2">{hit.user}</div>
+  
+                {/* Render the hit's tags */}
+                <p className="text-gray-700 text-base">Tags: {hit.tags}</p>
+  
+                {/* Render the hit's view count, download count, and like count */}
+                <div className="flex items-center mt-2 text-gray-500 text-xs">
+                  <EyeIcon className="h-4 w-4 mr-1" /> {/* View icon */}
+                  <span>{hit.views}</span> {/* View count */}
+                </div>
+                <div className="flex items-center mt-2 text-gray-500 text-xs">
+                  <ArrowDownTrayIcon className="h-4 w-4 mr-1" /> {/* Download icon */}
+                  <span>{hit.downloads}</span> {/* Download count */}
+                </div>
+                <div className="flex items-center mt-2 text-gray-500 text-xs">
+                  <HandThumbUpIcon className="h-4 w-4 mr-1" /> {/* Like icon */}
+                  <span>{hit.likes}</span> {/* Like count */}
+                </div>
+              </div>
+  
+              {/* Render the hit's tags as hashtags */}
+              <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-indigo-200 rounded-full px-3 py-1 text-sm font-semibold text-indigo-700 mr-2 mb-2">
+                  #{hit.tags.split(", ")[0]} {/* First tag */}
+                </span>
+                <span className="inline-block bg-indigo-200 rounded-full px-3 py-1 text-sm font-semibold text-indigo-700 mr-2 mb-2">
+                  #{hit.tags.split(", ")[1]} {/* Second tag */}
+                </span>
+              </div>
+  
+              {/* Render the user's profile picture and a download link for the hit */}
+              <div className="px-6 pt-4 pb-2 flex justify-between items-center">
+                {/* User's profile picture */}
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src={hit.userImageURL}
+                  alt={hit.user}
+                />
+  
+                {/* Download link */}
+                <a
+                  href={hit.largeImageURL} // Link to the hit's large image URL
+                  download // Download the image when the link is clicked
+                  className="text-blue-500 hover:underline" // Style the link
+                >
+                  <ArrowDownTrayIcon className="h-4 w-4 mr-1" /> {/* Download icon */}
+                  Download {/* Download text */}
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+  
+        {/* Pagination */}
+        <div className="mt-4 flex justify-center">
+          {/* If the current page is greater than 1, render a link to the previous page */}
+          {parseInt(page) > 1 && (
+            <a
+              href={`/search/${query}/${parseInt(page) - 1}`}
+              className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4 transition duration-200 ease-in-out"
+            >
+              Prev
+            </a>
+          )}
+  
+          {/* If there are 20 hits (the maximum), render a link to the next page */}
+          {data != null && data.hits.length == 20 && (
+            <a
+              href={`/search/${query}/${parseInt(page) + 1}`}
+              className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out"
+            >
+              Next
+            </a>
+          )}
+        </div>
+      </div>
+    );
+  }
+  
+  // Export the component as the default export
+  export default SearchQuery;
+  
--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,50 +1,107 @@
 'use client'
+// Import necessary hooks and modules from react and next/navigation packages
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
+// Declare the Search component
 const Search = () => {
-  const [query, setQuery] = useState("");
-  const router = useRouter();
+  // Declare state variables using useState hook
+  const [query, setQuery] = useState(""); // For storing search query
+  const [imageType, setImageType] = useState("all"); // For storing selected image type
+  const [category, setCategory] = useState(""); // For storing selected category
+  const [editorsChoice, setEditorsChoice] = useState(false); // For storing the state of Editor's Choice checkbox
+  const [safeSearch, setSafeSearch] = useState(false); // For storing the state of Safe Search checkbox
+  const [order, setOrder] = useState("popular"); // For storing the selected order type
+  const router = useRouter(); // For routing purposes
 
+  // Function to handle form submission
   const handleSearch = (e) => {
-    e.preventDefault();
-    router.push(`/search/${query}`);
+    e.preventDefault(); // Prevent default form submission
+
+    // Prepare search parameters string
+    let searchParameters = `${query}&image_type=${imageType}&category=${category}&editors_choice=${editorsChoice}&safesearch=${safeSearch}&order=${order}`;
+
+    // Route to the search page with the prepared parameters
+    router.push(`/search/${searchParameters}/1`);
   }
 
+  // Render the form
   return (
-    <>
-    <div className="flex items-center justify-center h-screen">
-      <div className="relative w-full max-w-xl mx-auto">
-        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-          <div className="w-full border-t border-gray-300"></div>
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <form onSubmit={handleSearch} className="bg-white p-6 rounded-lg shadow-lg w-full md:max-w-md">
+        <h2 className="text-2xl mb-4 text-center font-semibold text-gray-700">Image-Inn Search</h2>
+
+        {/* Input for search query */}
+        <div className="mb-4">
+          <label className="block text-gray-700">Search</label>
+          <input 
+            className="w-full px-3 py-2 border border-gray-300 rounded-md 
+            hover:border-gray-500 focus:outline-none focus:ring-2 
+            focus:ring-indigo-500" 
+            type="text" 
+            placeholder="Search"
+            value={query} 
+            onChange={e => setQuery(e.target.value)} // Update the query state variable on input change 
+          />
         </div>
-        <form onSubmit={handleSearch} className="relative flex justify-center text-sm leading-5">
-        <div className="relative flex justify-center text-sm leading-5">
-          <span className="px-2 text-gray-500 bg-white">
-            <input 
-              className="px-4 py-2 mr-2 border border-gray-300 rounded-md 
-              hover:border-gray-500 focus:outline-none focus:ring-2 
-              focus:ring-indigo-500" 
-              type="text" 
-              placeholder="Search"
-              value={query} 
-              onChange={e => setQuery(e.target.value)} 
-            />
-            <button 
-              className="px-4 py-2 text-white bg-indigo-500 rounded-md 
-              hover:bg-indigo-600 focus:outline-none focus:ring-2 
-              focus:ring-indigo-200"
-              onClick={handleSearch}
-            >
-              Search
-            </button>
-          </span>
+        
+        {/* Dropdown for image type selection */}
+        <div className="mb-4">
+          <label className="block text-gray-700">Image Type</label>
+          <select className="w-full border border-gray-300 rounded-md" onChange={e => setImageType(e.target.value)}>
+            <option value="all">All</option>
+            <option value="photo">Photo</option>
+            <option value="illustration">Illustration</option>
+            <option value="vector">Vector</option>
+          </select>
         </div>
-        </form>
-      </div>
+
+        {/* Dropdown for category selection */}
+        <div className="mb-4">
+          <label className="block text-gray-700">Category</label>
+          <select className="w-full border border-gray-300 rounded-md" onChange={e => setCategory(e.target.value)}>
+            <option value="">All</option>
+            <option value="fashion">Fashion</option>
+            <option value="nature">Nature</option>
+            <option value="backgrounds">Backgrounds</option>
+            {/* Add other categories here */}
+          </select>
+        </div>
+
+        {/* Checkbox for Editor's Choice */}
+        <div className="mb-4 flex items-center">
+          <label className="text-gray-700 mr-3">Editor's Choice</label>
+          <input type="checkbox" className="border-gray-300 rounded" onChange={e => setEditorsChoice(e.target.checked)} />
+        </div>
+
+        {/* Checkbox for Safe Search */}
+        <div className="mb-4 flex items-center">
+          <label className="text-gray-700 mr-3">Safe Search</label>
+          <input type="checkbox" className="border-gray-300 rounded" onChange={e => setSafeSearch(e.target.checked)} />
+        </div>
+
+        {/* Dropdown for order selection */}
+        <div className="mb-4">
+          <label className="block text-gray-700">Order By</label>
+          <select className="w-full border border-gray-300 rounded-md" onChange={e => setOrder(e.target.value)}>
+            <option value="popular">Popular</option>
+            <option value="latest">Latest</option>
+          </select>
+        </div>
+
+        {/* Search button */}
+        <button 
+          className="w-full py-2 text-white bg-indigo-500 rounded-md 
+          hover:bg-indigo-600 focus:outline-none focus:ring-2 
+          focus:ring-indigo-200"
+          onClick={handleSearch} // Call handleSearch function on click
+        >
+          Search
+        </button>
+      </form>
     </div>
-    </>
   )
 }
 
+// Export the Search component as the default export
 export default Search
